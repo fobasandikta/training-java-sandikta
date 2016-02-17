f48541c680b94a81fcdd04afb9c048a1efc048a9
fobasandikta <irmanjuliansyah10@gmail.com>
2016-02-17 22:35:36 +0700

    Hello wolrd

---
+++ M1/HelloDunia.java
@@ -0,0 +1,12 @@
+public class HelloDunia {
+	public static void main(String [] sandikta){
+	
+	String a = "Sandikta";
+
+		System.out.println("Hallo sandikta");
+		System.out.println("Hallo sandikta");
+		System.out.println("Hallo sandikta");
+		System.out.println("Hallo sandikta");
+		System.out.println("Ini adalah cara nampilin variable : "+ a);
+	}
+}
